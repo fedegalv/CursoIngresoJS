fbf3e83cb882ac9f5671d971c12e7c612ec8faf5
Federico Galván <40763749+fedegalv@users.noreply.github.com>
2018-07-02 18:34:12 -0300

    Add files via upload
    ejercicios resueltos 1,2 y 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,11 @@
 function mostrar()
 {
+	var string1 = prompt("Ingrese el precio:", "Precio");
+	var string2 = prompt("Ingrese el porcentaje de descuento:", "%");
+	var precio = parseFloat(string1);
+	var porcentaje = parseInt(string2) // convertimos de string a int
+	var descuento =(porcentaje / 100) * precio;
+	var total_con_descuento= precio - descuento;
+	var preciofinal = document.getElementById('elPrecioFinal').placeholder=total_con_descuento; // cambiamos el valor de placeholder a el valor del total con descuento
 
 }
