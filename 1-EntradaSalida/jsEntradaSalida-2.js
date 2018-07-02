561e46d32e289c8cf074354a3556242841c69af7
fedegv4991 <40763749+fedegv4991@users.noreply.github.com>
2018-07-02 12:05:54 -0300

    ejercicios 1 2 3
    3 files

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,12 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	//var person = prompt("Please enter your name", "Harry Potter");
+	//alert(person);
+
+	var nombre;
+	nombre =  prompt("Ingrese su nombre: ", "Nombre a ingresar");
+	alert(nombre);
 }
 
