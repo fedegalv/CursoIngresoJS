06bb075663977ad365ff0ce2ac819ef008322d1c
Federico Galván <40763749+fedegalv@users.noreply.github.com>
2018-07-03 23:30:47 -0300

    cinco.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
-
+	var planeta = prompt("Ingrese un planeta del sistema solar: ");
+	switch (planeta) {
+		case "mercurio":
+			alert("Acá hace más calor");
+			break;
+		case "venus":
+			alert("Aca hace mas calor");
+			break;
+		case "tierra":
+			alert("Aca vivimos");
+			break;
+		case "marte":
+			alert("Aca hace mas frio");
+			break;
+		case "jupiter":
+			alert("Aca hace mas frio");
+			break;
+		case "saturno":
+			alert("Aca hace mas frio");
+			break;
+		case "urano":
+			alert("Aca hace mas frio");
+			break;
+		case "neptuno":
+			alert("Aca hace mas frio");
+			break;
+		case "pluton":
+			alert("Aca hace mas frio");
+			break;
+		default:
+			alert("No es un planeta valido");
+			break;
+	}
 }
