9c164280b2a0caabdb0db41f24fdc1543ecb80c9
Federico Galván <40763749+fedegalv@users.noreply.github.com>
2018-07-04 12:03:34 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,7 @@
-function mostrar()
+function Mostrar()
 {
-  
+  var nombre = document.getElementById('elNombre').value; // asignamos a nombre el valor (value) dentro del elemento elNombre  que se encuentra en dos.html usando la funcion correspondiente 
+  var localidad = document.getElementById('laLocalidad').value; // asignamos a localidad el valor dentro del elemento laLocalidad que se encuentra en dos.html
+  alert("Usted es "+nombre+" y vive en la localidad de "+localidad);
 }
+
