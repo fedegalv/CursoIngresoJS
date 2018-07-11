1d18770d6d7b36fd1b2429e379cb02e94c1e4775
Federico Galván <40763749+fedegalv@users.noreply.github.com>
2018-07-11 12:17:59 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,28 +1,23 @@
-function mostrar()
+function Mostrar()
 {
-	var numero1 = prompt("Ingrese el primer numero: ");
-	var numero2 = prompt("Ingrese el segundo numero: ");
-	var numero1int = parseFloat(numero1);
-	var numero2int = parseFloat(numero2);
+	var num1,num2;
+	num1 = prompt("Ingrese numero 1: ");
+	num1 = parseInt(num1);
+	num2 = prompt("Ingrese numero 2: ");
+	num2 = parseInt(num2);
 
-	if (numero1 == numero2) {
-		var concatenado = numero1 + numero2;
-		alert(concatenado);
+	if (num1 == num2) {
+		alert(num1+""+num2);
 	}
-
-	if (numero1 > numero2) {
-		var resta = numero1int - numero2int;
-		alert(resta);
-
+	else if (num1 > num2) {
+		var resta = num1 - num2;
+		alert(resta)
 	}
-	if (numero1 < numero2) {
-		var suma = numero1int + numero2int;
+	else {
+		var suma = num1 + num2;
 		if (suma > 10) {
 			alert("La suma es "+suma+" y supero el 10");
 		}
-		else {
-			alert(suma);
-		}
-	} 
+	}
 
 }
