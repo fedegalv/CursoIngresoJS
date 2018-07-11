608bffcb2b6385b49371b0d485e1dfcb5a4661a6
Federico Galván <40763749+fedegalv@users.noreply.github.com>
2018-07-11 12:15:42 -0300

    update

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,9 +1,16 @@
-function mostrar()
+function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+switch (mesDelAño) {
+	case "Febrero":
+		alert("Este mes no tiene mas de 29 dias");
+		break;
+	default:
+		alert("Este mes tiene 30 o mas dias");
+}
 	
 	
 
