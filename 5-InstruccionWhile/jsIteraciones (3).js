9e37f1e14f9de07f3a650ae049cf06250f0bf77b
Federico Galván <40763749+fedegalv@users.noreply.github.com>
2018-07-23 12:24:28 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,12 @@
-function mostrar()
+function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+//var clave = prompt("ingrese el número clave.");
+do {
+	clave = prompt("ingrese el número clave.");
 
+}
+while(clave != "utn750")
 
+alert("Clave correcta");
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,14 @@
-function mostrar()
+function Mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	do {
+		var numero = prompt("ingrese un número entre 0 y 10.");
+		numero= parseInt(numero);
+	}
+	while (numero < 0 || numero > 9);
+
+	//alert(numero);
+	document.getElementById('Numero').value = numero;
 
 
 }//FIN DE LA FUNCIÓN
